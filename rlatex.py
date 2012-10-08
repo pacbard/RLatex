ead6acdb46e036220d5c9f808c5cd6c2ff88aadc
Pacbard <bardellie@gmail.com>
2012-10-08 17:56:48 -0400

    fixed indentation problems

--- rlatex.py
+++ rlatex.py
@@ -19,15 +19,15 @@ Usage: {0} [options] inputfile.tex
 
 Options:
 
-    -h, --help:         print this message
-    -s, --server:       the CLSI server to contact
-    -a, --api_url:      API URl on the server
-    -t, --token:        your token
-    -f, --file:         get login information from a file
-    -c, --compiler      sets the LaTeX Compiler
-    -o, --output        sets the LaTeX output file
-    -l, --log:          saves the log file
-    -d, --debug:      	debug version of the script
+    -h, --help:		 print this message
+	-s, --server:	   the CLSI server to contact
+	-a, --api_url:	  API URl on the server
+	-t, --token:		your token
+	-f, --file:		 get login information from a file
+	-c, --compiler	  sets the LaTeX Compiler
+	-o, --output		sets the LaTeX output file
+	-l, --log:		  saves the log file
+	-d, --debug:	  	debug version of the script
 
 Exclude http:// from the server URL, since `http://' will be prepended to
 by the script.
@@ -42,9 +42,9 @@ If any of the server, username, and password are omitted, you will be
 asked to provide them.
 
 The CLSI interface supports the following compilers and outputs:
-    pdflatex    pdf
-    latex       dvi, pdf, ps
-    xelatex     pdf
+	pdflatex	pdf
+	latex	   dvi, pdf, ps
+	xelatex	 pdf
 
 See the RLaTeX documentation for more details on usage and limitations
 of rlatex."""
@@ -72,18 +72,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 class FSM(object):
-	"""	Implements a finite state machine.
-		
-		Transitions are given as 4-tuples, consisting of an origin state, a target
-		state, a condition for the transition (given as a reference to a function
-		which gets called with a given piece of input) and a pointer to a function
-		to be called upon the execution of the given transition. 
-		
-		\var transitions holds the transitions
-		\var current_state holds the current state
-		\var current_input holds the current input
-		\var current_transition hold the currently active transition
-		"""
+	## Implements a finite state machine.
+	#
+	#  Transitions are given as 4-tuples, consisting of an origin state, a target
+	#  state, a condition for the transition (given as a reference to a function
+	#  which gets called with a given piece of input) and a pointer to a function
+	#  to be called upon the execution of the given transition. 
+	#
+	#  \var transitions holds the transitions
+	#  \var current_state holds the current state
+	#  \var current_input holds the current input
+	#  \var current_transition hold the currently active transition
+	
 	def __init__(self, start_state=None, transitions=[]):
 		self.transitions = transitions
 		self.current_state = start_state
@@ -97,10 +97,10 @@ class FSM(object):
 		self.transitions.append([from_state, to_state, condition, callback])
 
 	def makeTransition(self, input):
-	"""	Makes a transition based on the given input.
+		## Makes a transition based on the given input.
+		#
+		#  @param input input to parse by the FSM
 		
-		@param input input to parse by the FSM
-		"""
 		for transition in self.transitions:
 			[from_state, to_state, condition, callback] = transition
 			if from_state == self.current_state:
@@ -116,10 +116,10 @@ class FSM(object):
 
 class rlatex(object):
 	def __init__(self):
-	"""	Init function
+		## Init function
+		#
+		#  __init__() initializes the class variables to None or False
 		
-		__init__() initializes the class variables to None or False
-		"""
 		self.host, self.api_url, self.token, self.texsource, self.texpath = (None,) * 5
 		self.log, self.debug = (False,) *2
 		self.login = self.scriptPath()+'/login.txt'
@@ -127,11 +127,11 @@ class rlatex(object):
 		self.output = 'pdf'
 
 	def compile(self, argv):
-	"""	Compile function
+		## Compile function
+		#
+		#  Compile function.  This wraps the compile task
+		#  @param argv The command line arguments passed by the user
 		
-		Compile function.  This wraps the compile task
-		@param argv The command line arguments passed by the user
-		"""
 		self.manageArgv(argv)
 		
 		self.loadLogin()
@@ -141,16 +141,16 @@ class rlatex(object):
 		self.fetchResult(toDownload)
 	
 	def loadLogin(self):
-	"""	Loads login information or asks them through command line
-			
-		This function loads the default login file.  If the login file was
-		passed using the argument -f, it should just skip it.
-		You can provide a filename here and the script will read your login 
-		information from that file. The format must be:
-		 * server = 'foo.com'
-		 * api_url = 'clsi/compile'
-		 * token = 'token'
-		"""
+		## Loads login information or asks them through command line
+		#	
+		#  This function loads the default login file.  If the login file was
+		#  passed using the argument -f, it should just skip it.
+		#  You can provide a filename here and the script will read your login 
+		#  information from that file. The format must be:
+		#   * server = 'foo.com'
+		#   * api_url = 'clsi/compile'
+		#   * token = 'token'
+		
 		try:
 			with open(self.login, 'r') as f:
 				print('Login information found in file {0}.'.format(self.login))
@@ -171,15 +171,15 @@ class rlatex(object):
 			self.api_url = raw_input('Enter API URL: ')
 		if not self.token:
 			self.token = raw_input('Enter token:  ')
-
-		## Screen confirmation of the settings
+		
+		# Screen confirmation of the settings
 		print("Server: ", self.host)
 		print("API URL: ", self.api_url)
 		print("Token: ", self.token)
 	
 	def manageArgv(self, argv):
-	"""	Argument management
-		"""
+		## Argument management
+		
 		# Checks if all the flags are correct
 		try:
 			opts, file = getopt.getopt(argv[1:], 'hlds:a:t:f:l:c:o:',
@@ -226,15 +226,15 @@ class rlatex(object):
 		self.texsource = os.path.splitext(self.texsource)[0]
 		
 	def scriptPath(self):
-	"""	Returns the script installation path
+		## Returns the script installation path
+		#
+		#  scriptPath() returns the script path.
+		#  If the application is compiled with py2exe, it will use the
+		#  frozen status.
+		#  This function is used to load the login file from the installation
+		#  directory of the script.
+		#  @return string The script path
 		
-		scriptPath() returns the script path.
-		If the application is compiled with py2exe, it will use the
-		frozen status.
-		This function is used to load the login file from the installation
-		directory of the script.
-		@return string The script path
-		"""
 		if hasattr(sys, 'frozen'):
 			basis = sys.executable
 		else:
@@ -242,15 +242,14 @@ class rlatex(object):
 		return(os.path.split(basis)[0])
 
 	def do_request(self, xml_request):
-	"""	Posts the XML request
+		## Posts the XML request
+		#
+		#  do_request posts the XML request to the CLSI server.
+		#  This is based on HTTP XML Post request, by www.forceflow.be
+		#  It returns the server response to the request.
+		#  If the --debug flag is set, it will dump the XML request and the 
+		#  server response in the debug file.
 		
-		do_request posts the XML request to the CLSI server.
-		This is based on HTTP XML Post request, by www.forceflow.be
-		It returns the server response to the request.
-		If the --debug flag is set, it will dump the XML request and the 
-		server response in the debug file.
-		"""
-
 		webservice = httplib.HTTP(self.host)
 		webservice.putrequest("POST", self.api_url)
 		webservice.putheader("Host", self.host)
@@ -267,15 +266,15 @@ class rlatex(object):
 			logging.info("XML request result")
 			logging.info(result)
 		return result
-
+	
 	def buildRequest(self, path, source):
-	"""	Builds the XML request
+		## Builds the XML request
+		#
+		#  buildRequest() builds the XML request from the .tex source file.
+		#  As of version 0.3, it searches for included file in the source
+		#  file.  See findIncluded() documentation for more details.
+		#  command
 		
-		buildRequest() builds the XML request from the .tex source file.
-		As of version 0.3, it searches for included file in the source
-		file.  See findIncluded() documentation for more details.
-		command
-		"""
 		toCompile = [source+".tex"] + self.findIncluded(path+source+".tex")
 		
 		compile = Element("compile")
@@ -300,30 +299,36 @@ class rlatex(object):
 				
 			resource.text = "\n<![CDATA[\n"+cdata.decode('utf-8','ignore')+"\n]]>\n"
 		
-		string = ElementTree.tostring(compile, encoding='utf-8', method="xml").replace('&gt;', '>').replace('&lt;', '<').replace('&amp;', '&')
-
+		# Python version > 2.7
+		#string = ElementTree.tostring(compile, encoding="utf-8", method="xml").replace('&gt;', '>').replace('&lt;', '<').replace('&amp;', '&')
+		# Python version <= 2.6
+		string = ElementTree.tostring(compile, encoding="utf-8").replace('&gt;', '>').replace('&lt;', '<').replace('&amp;', '&')
+		
 		return string
 
 	def fetchResult(self, response):
-	"""	Parses the server response and downloads compiled files
+		## Parses the server response and downloads compiled files
+		#
+		#  fetchResult() parses the CLSI server XML response and downloads the
+		#  PDF file if the CLSI was able to compile the input file.  The log file
+		#  is automatically deleted, unless the --log flag is passed to the script.
+		#  If not, it downloads the log file and terminates the execution of the script.
 		
-		fetchResult() parses the CLSI server XML response and downloads the
-		PDF file if the CLSI was able to compile the input file.  The log file
-		is automatically deleted, unless the --log flag is passed to the script.
-		If not, it downloads the log file and terminates the execution of the script.
-		"""
 		filename = self.texsource
-	
+		
 		tree = ElementTree.fromstring(response)
 		for child in tree.getchildren():
 			if child.tag == 'status':
 				result = child.text
+				# Compilation was successful
 				if result == 'success':
 					for child in tree.getchildren():
 						if child.tag == 'output':
 							for child2 in child.getchildren():
+								logging.info("Saving output in :"+filename+"."+self.output)
 								output = child2.get('url')
 								urllib.urlretrieve (output, filename+"."+self.output)
+				# Compilation not successful
 				else:
 					for child in tree.getchildren():
 						if child.tag == 'error':
@@ -344,25 +349,26 @@ class rlatex(object):
 			if child.tag == 'logs':
 				for child2 in child.getchildren():
 					logs = child2.get('url')
-					urllib.urlretrieve (logs, filename+".log")
-					log = open(filename+".log","r").read()
-					if not self.log:
-						os.remove(filename+".log")
-					print(log)
+					log = urllib.urlopen(logs)
+					print(log.read())
+					if self.log:
+						localFile = open(filename+".log", 'w')
+						localFile.write(log.read())
+						localFile.close()
 
 	def findIncluded(self, file):
-	"""	Finds the included files
+		## Finds the included files
+		#
+		#  findIncluded() scans the source file for included files.
+		#  At this time (v0.3), it supports \input, \include, \includeonly, \thebibliography 
+		#  commands.
+		#  If the included file is required by the LaTeX compiler but does not need to be
+		#  included in the .tex file (e.g., a .sty file) use the command %\include{my.sty}
+		#  to pass the my.sty file to the CLSI compiler.
+		#  If no file extension is passes, this function will append the file extension
+		#  .tex to files included with the commands \input, \include, and \includeonly
+		#  and the file extension .bib for the files included with \thebibliography.
 		
-		findIncluded() scans the source file for included files.
-		At this time (v0.3), it supports \input, \include, \includeonly, \thebibliography 
-		commands.
-		If the included file is required by the LaTeX compiler but does not need to be
-		included in the .tex file (e.g., a .sty file) use the command %\include{my.sty}
-		to pass the my.sty file to the CLSI compiler.
-		If no file extension is passes, this function will append the file extension
-		.tex to files included with the commands \input, \include, and \includeonly
-		and the file extension .bib for the files included with \thebibliography.
-		"""
 		load_profile = open(file, "r")
 		read_it = load_profile.read()
 		myFiles = []
@@ -407,10 +413,10 @@ class rlatex(object):
 		return myFiles
 	
 def main():
-"""	Main function """
+	## Main function 
 
 	fsm = rlatex()
 	fsm.compile(sys.argv)
 
 if __name__ == "__main__":
-    main()
+	main()
